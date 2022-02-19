8956447432c1724949c10a8e1df02791c31a3faf
aswathselvam <m.aswath08gmail.com>
2022-02-19 03:52:08 -0500

    test

--- space.h
+++ space.h
@@ -97,7 +97,7 @@ class Space{
         void init();
         bool solve();
         bool checkCollision(NodeDim& node);
-        __global__ void cudaCheckCollision(ObstacleDim* d_obstacles, NodeDim node);
+        __global__ void cudaCheckCollision(ObstacleDim*, NodeDim);
         NodeDim start;
         NodeDim goal;
         NodeDim& goalnode;
